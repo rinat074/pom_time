2777c3fda3b5b80e0d666be579c65f2d75d8a025
Rinat <rinat7716000@gmail.com>
2025-01-25 12:18:29 +0300

    add new settings

--- settings.py
+++ settings.py
@@ -1,10 +1,15 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    app_title: str = "Fast API Lessons"
-    app_description: str = "Fast API Lessons"
-    app_version: str = "0.0.1"
     GOOGLE_TOKEN_ID: str = "1234567890"
-    sqlite_db_name: str = "pomodoro.sqlite"
+    DB_HOST: str = "0.0.0.0"
+    DB_PORT: int = 5432
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "password"
+    DB_NAME: str = "pomodoro"
+    CACHE_HOST: str = "0.0.0.0"
+    CACHE_PORT: int = 6379
+    CACHE_PASSWORD: str = "password"
+    CACHE_DB: int = 0
 
 settings = Settings()
